d247b6b1a2a2b7830c0f3db26560d0cf67d0fb30
Arkadip <arkadip671@gmail.com>
2025-09-07 04:07:36 +0530

    setup fetching names experimenting with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <>
-      Hello
-    </>
-  )
+interface Pokemon {
+  name: string;
+  url: string;
 }
 
-export default App
+const PokemonNames = () => {
+  const [pokeList, setPokeList] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const res = await axios.get<{ results: Pokemon[] }>(
+          "https://pokeapi.co/api/v2/pokemon/?limit=100000&offset=0"
+        );
+        const names = res.data.results.map((p) => p.name);
+        setPokeList(names);
+      } catch (err) {
+        console.error("error fetching pokemon.", err);
+      }
+    };
+    fetchPokemon();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <p>{pokeList.map((name, i) => (
+          <li key={i}>{name}</li>
+        ))}</p>
+      </div>
+    </>
+  );
+};
+
+export default PokemonNames;
